a31acfe5c9b3acbf51c9383a09003611b64ea16b
likai <1195419506@qq.com>
2021-09-06 00:39:47 +0800

    fix: 优化剪绳子的实现代码
    完善每一步的注释

--- src/DynamicProgramming.ts
+++ src/DynamicProgramming.ts
@@ -7,36 +7,52 @@ export default class DynamicProgramming {
    *
    */
   public cutTheRope(length: number): number {
-    // 创建结果数组并全部初始化为0
-    const products = new Array<number>(length + 1);
-    for (let i = 0; i < products.length; i++) {
-      products[i] = 0;
-    }
     // 由于绳子只能整数切，所以绳子长度小于2时，无法进行裁剪
     if (length < 2) {
-      products[0] = 0;
-      products[1] = 0;
+      return 0;
     }
     // 绳子长度为2时，只能从中间裁剪, 所有切法的最大乘积为1
     if (length === 2) {
-      products[2] = 1;
+      return 1;
     }
     // 绳子长度为3时，可以切成:
     // 1. 1 1 1
     // 2. 1 2
     // 1 * 2 > 1 * 1 * 1, 故长度为3时, 所有切法的最大乘积为2
     if (length === 3) {
-      products[3] = 2;
+      return 2;
     }
 
+    // 创建结果数组，存储每段长度绳子切分时的最大乘积
+    const products = new Array<number>(length + 1);
+    // 长度小于4时，绳子的最大乘积我们已经推算出来了，因此直接保存即可
+    products[0] = 0;
+    products[1] = 1;
+    // 绳子长度为2或3时，不进行拆分，最大乘积为绳子的长度
+    products[2] = 2;
+    products[3] = 3;
+
+    // 绳子长度大于4时需要对绳子进行切分，求出切分后的每段绳子的最大乘积
     for (let i = 4; i <= length; i++) {
-      for (let j = 1; j < i; j++) {
-        products[i] = Math.max(
-          products[i],
-          Math.max(j, products[j]) * Math.max(i - j, products[i - j])
-        );
+      // 赋初始值
+      products[i] = 0;
+      // 当前长度绳子的最大乘积
+      let max = 0;
+      // 至少切一刀，从绳子的1位置开始切，切到i/2位置。
+      // 求出长度为i时，切一刀后两段绳子的最大乘积
+      for (let j = 1; j <= i / 2; j++) {
+        // 根据递推公式求当前裁剪位置的两段绳子的乘积
+        const product = products[j] * products[i - j];
+        // 比对历史切法中两段绳子的最大乘积和当前切法两段绳子的乘积
+        if (max < product) {
+          // 替换最大值
+          max = product;
+        }
+        // 修改当前绳子长度切法的最大乘积
+        products[i] = max;
       }
     }
+    // 每种长度绳子的最大乘积都已经求出，length位置的值就是整个问题的解
     return products[length];
   }
 }
