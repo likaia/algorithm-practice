bc56e71fc0d0f20c646621266c426ab283f6ef09
likai <1195419506@qq.com>
2022-09-14 20:38:08 +0800

    fix: 完善测试用例

--- src/test-case/stackPushAndPopSequence-test.ts
+++ src/test-case/stackPushAndPopSequence-test.ts
@@ -1,7 +1,8 @@
 import { StackPushAndPopSequence } from "../StackPushAndPopSequence.ts";
 
 const pushSuite = [1, 2, 3, 4, 5];
-// const popSuite = [4, 5, 3, 2, 1];
-const popSuite = [4, 3, 5, 1, 2];
-const result = StackPushAndPopSequence(pushSuite, popSuite);
-console.log(result);
+const popSuite1 = [4, 5, 3, 2, 1];
+const popSuite2 = [4, 3, 5, 1, 2];
+const result1 = StackPushAndPopSequence(pushSuite, popSuite1);
+const result2 = StackPushAndPopSequence(pushSuite, popSuite2);
+console.log(result1, result2);
